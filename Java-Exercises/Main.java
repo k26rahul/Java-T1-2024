2eb52aae0da82fe0bdac6d0f4f551a1c32c4f7f7
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2024-03-13 22:55:38 +0530

    Create Main.java

---
+++ Java-Exercises/Main.java
@@ -0,0 +1,112 @@
+
+// import java.io.Serializable;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.Collection;
+// import java.util.LinkedList;
+// import java.util.List;
+// import java.util.RandomAccess;
+// import java.util.ArrayDeque;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.Queue;
+// import java.util.stream.Collector;
+// import java.util.stream.Collectors;
+
+/**
+ * Main
+ */
+public class Main {
+  public static void main(String[] args) {
+    // List<Character> list = "hello".chars().mapToObj(e -> (char)
+    // e).collect(Collectors.toList());
+    // System.out.println(list);
+    // List<Character> chars = "hello";
+    // Queue<Integer> queue = new ArrayDeque<>();
+    // System.out.println(queue.hashCode());
+    // System.out.println("hello");
+    // List.of("hello".toCharArray()).forEach(letter -> {
+    // System.out.println(letter instanceof char[]);
+    // System.out.println(letter[0]);
+    // });
+
+    // System.out.println(Arrays.asList(array));
+    // System.out.println(Arrays.asList(array));
+    // System.out.println(new ArrayList<>(Arrays.asList(array)));
+
+    // String[] array = {"Apple", "Banana", "Orange"};
+    // System.out.println("hello".toCharArray());
+    // System.out.println(new
+    // ArrayList<>(Arrays.asList("hello".toCharArray())).get(0));
+    // System.out.println(List.of("hello".toCharArray()));
+    // System.out.println(Arrays.toString("hello".toCharArray()));
+    // System.out.println(Arrays.toString("hello".toCharArray()));
+    // queue.forEach((x) -> {
+    // System.out.println(x);
+    // });
+  }
+
+  // public static void lister(List list) {
+  // System.out.println(list instanceof List<Object>);
+  // List<String> list2 = (List<String>) list;
+  // String boss = list2.get(0).toLowerCase();
+  // System.out.println(boss);
+  // System.out.println(list);
+  // }
+
+  // public static void main(String[] args) {
+  // // Raw List
+  // List rawList = new ArrayList();
+  // rawList.add("hello"); // Compiles fine, but not type-safe
+  // rawList.add(42); // Compiles fine, but not type-safe
+
+  // // List<Object>
+  // List<Object> objectList = new ArrayList<>();
+  // objectList.add(5); // Compiles fine, type-safe
+  // objectList.add(42); // Compiles fine, type-safe
+
+  // // Accessing elements
+  // String str1 = (String) rawList.get(0); // Unsafe cast, may throw
+  // ClassCastException
+  // String str2 = (String) objectList.get(0); // Safe cast, known to contain
+  // String
+
+  // System.out.println(str1);
+  // System.out.println(str2);
+  // }
+
+  // public static void main(String[] args) {
+  // // lister(new ArrayList<>(Arrays.asList("hello", "world")));
+  // // lister(new ArrayList<>(Arrays.asList(1, 2, 3)));
+  // // int[] array = new int[] {1, 2, 3};
+  // // System.out.println(array instanceof int[]);
+  // // int[][] array2d = new int[][] {{1, 2, 3}, {1, 1}};
+  // // System.out.println(Arrays.toString());
+  // // System.out.println(array2d[0][2]);
+  // // char letter = (char) 65;
+  // // int number = (int) letter;
+  // // System.out.println(letter);
+  // // System.out.println(number);
+
+  // // char letter = "hello".charAt(0);
+  // // System.out.println(letter);
+  // // System.out.println("hello".toCharArray()[4]);
+
+  // // System.out.println(letter == 'A');
+  // // System.out.println("hello" == "hello");
+  // // String str = String.valueOf(100) + "hello";
+  // // System.out.println(str.charAt(3));
+  // // System.out.println(String.valueOf(100).equals("100"));
+  // // char letter = 'A';
+  // // System.out.println(String.valueOf(letter).toLowerCase().charAt(0));
+  // // List<String> list = new ArrayList<>(new LinkedList<>());
+  // // list.add("hello");
+  // // System.out.println(list);
+  // // System.out.println(Arrays.asList("hello", "bye") instanceof List);
+  // // System.out.println(Arrays.asList("hello", "bye") instanceof Collection);
+  // // System.out.println(Arrays.asList("hello", "bye") instanceof Serializable);
+  // // System.out.println(Arrays.asList("hello", "bye") instanceof RandomAccess);
+  // // System.out.println(Arrays.asList("hello", "bye") instanceof ArrayList);
+  // }
+}
